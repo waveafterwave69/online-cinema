ada6c563c63c89e8315a345f7c6802e532f1cd0c
Michael Shonov <waveafterwave3w2@gmail.com>
2025-08-12 07:28:23 +0300

    add window scroll while open page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,9 +1,12 @@
 import { useSelector } from 'react-redux'
 import ProfileContent from '../../components/ProfileComponents/ProfileContent/ProfileContent'
+import { useEffect } from 'react'
 
 const ProfilePage: React.FC = () => {
     const { profile }: any = useSelector((state) => state)
-    console.log(profile)
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
 
     return (
         <>
