ea23203d18988162b184b07e8eead73b98c1f2c8
Michael Shonov <waveafterwave3w2@gmail.com>
2025-08-13 16:10:40 +0300

    add "add to favorites" logic

--- src/components/FilmPageComponents/FilmPageListItem/FilmPageListItem.tsx
+++ src/components/FilmPageComponents/FilmPageListItem/FilmPageListItem.tsx
@@ -2,30 +2,57 @@ import React from 'react'
 import styles from './FilmPageListItem.module.css'
 import { motion } from 'framer-motion'
 import type { Films } from '../../../types'
+import { toggleFav } from '../../../store/slices/profileSlice/profileSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 interface TrendsItemProps {
     film: Films
 }
 
 const FilmPageListItem: React.FC<TrendsItemProps> = ({ film }) => {
+    const dispatch = useDispatch()
+    const { profile } = useSelector((state: any) => state)
+
+    const addToFav = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.stopPropagation()
+        dispatch(toggleFav(film))
+    }
+
+    const isFavorite = profile.fav.filter(
+        (el: Films) => el.kinopoiskId === film.kinopoiskId
+    )
+
     return (
         <>
-            <a
-                href={
-                    film.kinopoiskId
-                        ? `/movie/${film.kinopoiskId}`
-                        : `/movie/${film.filmId}`
-                }
-            >
-                <motion.li className={styles.item}>
-                    <button className={styles.item__button}>+</button>
+            <motion.li className={styles.item}>
+                <motion.button
+                    whileHover={{
+                        backgroundColor: '#EC5BAA',
+                    }}
+                    style={{
+                        backgroundColor:
+                            isFavorite.length > 0 ? '#EC5BAA' : 'transparent',
+                    }}
+                    transition={{ duration: 0.1 }}
+                    className={styles.item__button}
+                    onClick={addToFav}
+                >
+                    +
+                </motion.button>
+                <a
+                    href={
+                        film.kinopoiskId
+                            ? `/movie/${film.kinopoiskId}`
+                            : `/movie/${film.filmId}`
+                    }
+                >
                     <img
                         src={film.posterUrlPreview}
                         alt={film.nameOriginal}
                         className={styles.item__image}
                     />
-                </motion.li>
-            </a>
+                </a>
+            </motion.li>
         </>
     )
 }
--- src/components/TrendsItem/TrendsItem.tsx
+++ src/components/TrendsItem/TrendsItem.tsx
@@ -2,30 +2,57 @@ import React from 'react'
 import type { Films } from '../../types'
 import styles from './TrendsItem.module.css'
 import { motion } from 'framer-motion'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleFav } from '../../store/slices/profileSlice/profileSlice'
 
 interface TrendsItemProps {
     film: Films
 }
 
 const TrendsItem: React.FC<TrendsItemProps> = ({ film }) => {
+    const dispatch = useDispatch()
+    const { profile } = useSelector((state: any) => state)
+
+    const addToFav = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.stopPropagation()
+        dispatch(toggleFav(film))
+    }
+
+    const isFavorite = profile.fav.filter(
+        (el: Films) => el.kinopoiskId === film.kinopoiskId
+    )
+
     return (
         <>
-            <a
-                href={
-                    film.kinopoiskId
-                        ? `/movie/${film.kinopoiskId}`
-                        : `/movie/${film.filmId}`
-                }
-            >
-                <motion.li className={styles.item}>
-                    <button className={styles.item__button}>+</button>
+            <motion.li className={styles.item}>
+                <motion.button
+                    whileHover={{
+                        backgroundColor: '#EC5BAA',
+                    }}
+                    style={{
+                        backgroundColor:
+                            isFavorite.length > 0 ? '#EC5BAA' : 'transparent',
+                    }}
+                    transition={{ duration: 0.1 }}
+                    className={styles.item__button}
+                    onClick={addToFav}
+                >
+                    +
+                </motion.button>
+                <a
+                    href={
+                        film.kinopoiskId
+                            ? `/movie/${film.kinopoiskId}`
+                            : `/movie/${film.filmId}`
+                    }
+                >
                     <img
                         src={film.posterUrlPreview}
                         alt={film.nameOriginal}
                         className={styles.item__image}
                     />
-                </motion.li>
-            </a>
+                </a>
+            </motion.li>
         </>
     )
 }
